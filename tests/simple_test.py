e45e9c4b6099dcc034215f14665a7d182b986f69
skojaku <skojaku@iu.edu>
2021-06-27 16:56:36 -0400

    update test

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -3,28 +3,18 @@ import sys
 import unittest
 
 import numpy as np
-from scipy import sparse
-import pandas as pd
-
 import cidre
 
 
 class Test(unittest.TestCase):
     def setUp(self):
-        df = pd.read_csv("../data/edges.csv")
-        src = df["source"].values
-        trg = df["target"].values
-        w = df["weight"].values
-        self.N = np.maximum(np.max(src), np.max(trg)) + 1
-        self.A = sparse.csr_matrix((w, (src, trg)), shape=(self.N, self.N))
+        A, node_labels = cidre.utils.read_edge_list("data/synthe/edge-table.csv")
+        self.N = A.shape[0] 
+        self.A = A 
         self.group_membership = np.random.randint(0, 3, self.N)
 
-    def test_edge_filtering(self):
-        efilter = cidre.EdgeFilter()
-        efilter.fit(self.A, group_membership=self.group_membership)
-
     def test_detect(self):
-        model = cidre.CIDRE(group_membership=self.group_membership)
+        model = cidre.Cidre(group_membership=self.group_membership)
         groups = model.detect(self.A, threshold=0.15)
 
 
