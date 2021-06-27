2c0a99fa9e472e85a92d562ffea8d6e919f88f71
skojaku <skojaku@iu.edu>
2021-06-27 14:31:43 -0400

    add synthe data

---
+++ data/synthe/gen_synthetic_data.py
@@ -0,0 +1,130 @@
+# %%
+import numpy as np
+from scipy import sparse
+import pandas as pd
+
+#
+# Baseline
+#
+def gen_deg(alpha, dave, N):
+    deg = 1 / np.random.power(alpha, N)
+    return deg * dave * N / np.sum(deg)
+
+
+def gen_noisy_dcSBM(indeg, outdeg, gids, mix_rate, is_bipartite=False):
+    N = indeg.size
+    K = np.max(gids) + 1
+
+    U = sparse.csr_matrix(
+        (np.ones_like(gids), (np.arange(gids.size), gids)), shape=(N, K)
+    )
+    Din = np.array(indeg.T @ U).reshape(-1)
+    Dout = np.array(outdeg.T @ U).reshape(-1)
+
+    Arand = np.outer(indeg, outdeg) / np.sum(indeg)
+    if is_bipartite:
+        Acom = (
+            sparse.diags(outdeg / np.maximum(Dout[gids], 1))
+            @ U
+            @ np.array([[0, np.sum(Dout)], [np.sum(Din), 0]])
+            @ U.T
+            @ sparse.diags(indeg / np.maximum(Din[gids], 1))
+        )
+    else:
+        Acom = (
+            sparse.diags(outdeg / np.maximum(Dout[gids], 1))
+            @ U
+            @ sparse.diags((Dout + Din) / 2)
+            @ U.T
+            @ sparse.diags(indeg / np.maximum(Din[gids], 1))
+        )
+    A = mix_rate * Arand + (1 - mix_rate) * Acom
+    A = A - np.diag(np.diag(A))
+    return np.random.poisson(A)
+
+
+def gen_anomalous_group(
+    nodes, N, Nd, indeg_base, outdeg_base, inflation_rate, mix_rate
+):
+    Nc = len(nodes)
+    donors = np.random.choice(Nc, Nd, replace=False)
+    recipients = np.array(list(set(np.arange(Nc)).difference(donors)))
+    Nr = Nc - Nd
+
+    indeg = np.zeros_like(nodes)
+    outdeg = np.zeros_like(nodes)
+    gids = np.zeros_like(nodes)
+
+    theta = inflation_rate / (1 - inflation_rate)
+
+    indeg[recipients] = indeg_base[nodes[recipients]] * theta
+    outdeg[donors] = outdeg_base[nodes[donors]] * theta
+
+    alpha = np.sum(indeg) / np.sum(outdeg)
+    if alpha > 1:
+        outdeg = outdeg * alpha
+    else:
+        indeg = indeg / alpha
+
+    gids[recipients] = 1
+
+    B = gen_noisy_dcSBM(indeg, outdeg, gids, mix_rate, is_bipartite=True)
+    U = sparse.csr_matrix(
+        (np.ones_like(gids), (nodes, np.arange(gids.size))), shape=(N, Nc)
+    )
+    return U @ B @ U.T
+
+
+if __name__ == "__main__":
+    output_node_file = "node-table.csv"
+    output_edge_file = "edge-table.csv"
+
+    N = 100  # Number of nodes
+    dave = 100  # average degree
+    K = 2  # Number of communities
+    mix_rate = 0.1  # Level of noise
+
+    Kc = 3  # Number of anomalous groups
+    Nc = 5  # Number of nodes in an anomalous group
+    Nd = 2  # Number of donors
+    inflation_rate = 0.3  # fraction of edge weights inflated by anomalous groups
+
+    # %% Generate base graph
+    outdeg = gen_deg(3, dave, N)
+    indeg = gen_deg(3, dave, N)
+    alpha = np.sum(indeg) / np.sum(outdeg)
+    if alpha > 1:
+        outdeg = outdeg * alpha
+    else:
+        indeg = indeg / alpha
+    gids = np.random.randint(0, K, N)
+    Abase = gen_noisy_dcSBM(indeg, outdeg, gids, mix_rate)
+
+    indeg = np.array(Abase.sum(axis=0))
+    outdeg = np.array(Abase.sum(axis=1))
+
+    # %% Generate anomalous groups
+    nodes = np.arange(N)
+    group_ids = np.ones(N) * np.nan
+    A = Abase.copy()
+    for k in range(Kc):
+        while True:
+            _nodes = np.random.choice(nodes, Nc, replace=False)
+            if len(set(gids[_nodes])) == 1:
+                break
+
+        Ak = gen_anomalous_group(_nodes, N, Nd, indeg, outdeg, inflation_rate, 0)
+        A += Ak
+        nodes = nodes[~np.isin(nodes, _nodes)]
+        group_ids[_nodes] = k
+
+    pd.DataFrame(
+        {"id": np.arange(N), "group_id": gids, "anomalous_group_id": group_ids}
+    ).to_csv(output_node_file)
+
+    r, c, v = sparse.find(A)
+    pd.DataFrame({"src": r, "trg": c, "w": v}).to_csv(output_edge_file)
+
+# %%
+
+# %%
