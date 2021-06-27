b8274e8bbe5d9ac902e79ce2ee5444a8a058bb2e
skojaku <skojaku@iu.edu>
2021-06-27 16:53:04 -0400

    clean up

--- data/faculty_hiring/make_data.sh
+++
@@ -1,3 +0,0 @@
-wget https://aaronclauset.github.io/facultyhiring/ComputerScience_vertexlist.txt
-wget https://aaronclauset.github.io/facultyhiring/ComputerScience_edgelist.txt
-python3 preproecss.py
